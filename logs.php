61d9e070084c677348bb5b63b877680e2377f4f9
すー <s1732087AP@s.chibakoudai.jp>
2019-04-15 11:02:01 +0900

    init

---
+++ logs.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 結果をjsonで返却する
+ *
+ * @param  array resultArray 返却値
+ * @return string jsonで表現されたレスポンス
+ * @author kobayashi
+ **/
+function returnJson($resultArray){
+  if(array_key_exists('callback', $_GET)){
+    $json = $_GET['callback'] . "(" . json_encode($resultArray) . ");";
+  }else{
+    $json = json_encode($resultArray);
+  }
+  header('Content-Type: text/html; charset=utf-8');
+  echo  $json;
+  exit(0);
+}
+
+
+
+/**
+ * ユーザの一覧をjsonで返す
+ *
+ * @param string user_type  a,admin,o,operatorg,guest,のいずれか
+ * @return array
+ *          string result   OK,NG
+ *          array  users   成功時のみ。ユーザリスト
+ *              string name ユーザ名
+ *              int age 年齢
+ *          string message  失敗時のみ。エラーメッセージ
+ *
+ * @author kobayashi
+ **/
+
+$result = [];
+
+try {
+  //  値の検証
+//   if (empty($type)) {
+//     throw new RuntimeException("no type...");
+//   }
+
+  $result = [
+      ['result' => 'ok'],
+  ];
+
+  returnJson($result);
+} catch (RuntimeException $e) {
+  header("HTTP/1.1 400 Bad Reques");
+  exit(0);
+} catch (Exception $e) {
+  header("HTTP/1.1 500 Internal Server Error");
+  exit(0);
+}
